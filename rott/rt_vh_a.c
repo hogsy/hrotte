11748fe8c5205475ae2d1d18e76b28ae63891fb1
relnev <relnev@1d3a2ae3-721e-0410-8303-f7269feeb052>
2002-12-21 06:56:34 +0000

    rt_vh_a.c: stub version of rt_vh_a.asm

---
+++ rott/rt_vh_a.c
@@ -0,0 +1,16 @@
+#include "rt_def.h"
+
+#include "rt_vh_a.h"
+
+/* C version of rt_vh_a.asm */
+
+void VH_UpdateScreen (void)
+{
+	STUB_FUNCTION;
+}
+
+void JoyStick_Vals (void)
+{
+	STUB_FUNCTION;
+}
+
