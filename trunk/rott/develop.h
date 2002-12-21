fbadfa8ca343873097ab62623e83e59ee1071e92
relnev <relnev@1d3a2ae3-721e-0410-8303-f7269feeb052>
2002-12-21 00:19:56 +0000

    rt_vid.c: moved dos headers, stubbed a few low-level functions
    rt_vh_a.h: #ifdef'd a watcom assembly pragma
    
    develop.h: got rid of a "multiline comment"

--- trunk/rott/develop.h
+++ trunk/rott/develop.h
@@ -63,9 +63,11 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
    }
 
 #define waminot()
-//   {                  \
-//   programlocation=-1;\
-//   }
+/*
+   {                  \
+   programlocation=-1;\
+   }
+*/
 
 #else
 
