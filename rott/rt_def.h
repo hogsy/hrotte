442d24eae3b1820b7c1a817aaf88dff869621614
relnev <relnev@1d3a2ae3-721e-0410-8303-f7269feeb052>
2002-12-22 03:37:13 +0000

    converted boolean to unsigned char, due to watcom c serialization

--- rott/rt_def.h
+++ rott/rt_def.h
@@ -246,12 +246,21 @@ typedef long fixed;
 
 //////////////////      GLOBAL ENUMERATED TYPES    ///////////////////////
 
-
+#ifdef __WATCOMC__
 typedef enum
  {false,
   true
  }
  boolean;
+#else
+/* boolean is serialized at the moment, and watcomc made it a byte. */
+
+typedef unsigned char boolean;
+enum {
+  false, true
+};
+#endif
+
 
 
 
