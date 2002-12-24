3447adc2a218532a7d1a8f98182d5938045484a6
theoddone33 <theoddone33@1d3a2ae3-721e-0410-8303-f7269feeb052>
2002-12-24 05:48:29 +0000

    Fix status bar

--- rott/rt_game.c
+++ rott/rt_game.c
@@ -1825,10 +1825,17 @@ void DrawPPic (int xpos, int ypos, int width, int height, byte *src, int num, bo
    int k;
    int amt;
 
+#ifdef DOS
    if (up)
       amt = 2;
    else
       amt = -2;
+#else
+   if (up)
+      amt = 8;
+   else
+      amt = -8;
+#endif
 
    mask = 1;
 
