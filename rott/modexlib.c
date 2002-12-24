7487769125850014d5711c081f166c7cf8124662
theoddone33 <theoddone33@1d3a2ae3-721e-0410-8303-f7269feeb052>
2002-12-24 09:23:48 +0000

    yeah, good idea

--- rott/modexlib.c
+++ rott/modexlib.c
@@ -641,6 +641,13 @@ void VL_DePlaneVGA (void)
 }
 
 
+/* C version of rt_vh_a.asm */
+
+void VH_UpdateScreen (void)
+{
+	SDL_UpdateRect (SDL_GetVideoSurface (), 0, 0, 0, 0);
+}
+
 /*
 =================
 =
