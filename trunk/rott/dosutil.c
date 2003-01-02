c267894fa32e8ad25435f7d8245400c83cb708ea
relnev <relnev@1d3a2ae3-721e-0410-8303-f7269feeb052>
2003-01-02 22:20:22 +0000

    keep darkwar files in a separate directory

--- trunk/rott/dosutil.c
+++ trunk/rott/dosutil.c
@@ -128,7 +128,17 @@ int setup_homedir (void)
 #if PLATFORM_UNIX
 	int err;
 
+	/* try to create the root directory */
 	snprintf (ApogeePath, sizeof (ApogeePath), "%s/.rott/", getenv ("HOME"));
+	err = mkdir (ApogeePath, S_IRWXU);
+	
+/* keep the shareware and registered game data separated */
+#if (SHAREWARE == 1)
+	snprintf (ApogeePath, sizeof (ApogeePath), "%s/.rott/", getenv ("HOME"));
+#else
+	snprintf (ApogeePath, sizeof (ApogeePath), "%s/.rott/darkwar/", getenv ("HOME"));
+#endif
+
 	err = mkdir (ApogeePath, S_IRWXU);
 	if (err == -1 && errno != EEXIST)
 	{
