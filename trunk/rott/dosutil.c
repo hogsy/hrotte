2018ff34d85c1a7825aa54b2c7e8cbcbf436fb2f
fabian <fabian@1d3a2ae3-721e-0410-8303-f7269feeb052>
2009-03-30 08:25:06 +0000

    Fix building under MinGW:  * mkdir() does only accept one argument  * EXECINFO is not supported

--- trunk/rott/dosutil.c
+++ trunk/rott/dosutil.c
@@ -125,7 +125,7 @@ extern char ApogeePath[256];
 
 int setup_homedir (void)
 {
-#if PLATFORM_UNIX
+#if PLATFORM_UNIX && !defined(__MINGW32__)
 	int err;
 
 	/* try to create the root directory */
@@ -197,7 +197,7 @@ void DisplayTextSplash(byte *text, int l)
 	printf ("\033[m");
 }
 
-#if (USE_EXECINFO == 1)
+#if (USE_EXECINFO == 1) && !defined(__MINGW32__)
 #include <execinfo.h>
 
 void print_stack (int level)
